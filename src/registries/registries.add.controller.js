426d02ef8b107ab8491a4822ab4244fc86142cdd
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-06-15 23:41:39 +0300

    Improve to a more generic field type

--- src/registries/registries.add.controller.js
+++ src/registries/registries.add.controller.js
@@ -6,44 +6,24 @@
     .controller('RegistriesAddCtrl', RegistriesAddCtrl);
 
   function RegistriesAddCtrl() {
-    this.message = 'test';
-
-
-    //
-    // name: {type: String},
-    // description: {type: String},
-    // createdBy: {type: String},
-    // creationDate: { type: Date, default: Date.now },
-    // status: {type: String, default: "DRAFT" },
-    // nbrFields: {type: Number},
-    // nbrRecords: {type: Number, default: 0 },
-    // fields: { type: Array },
-    // records: { type: Array }
-
 
     this.registry = {
       name: '',
       description: '',
       status: '',
-      administrativeFields: [],
-      clinicalFields: []
+      fields: []
     };
 
-    this.addAdministrativeField = function() {
-      this.registry.administrativeFields.push({
-        name: '',
-        value: '',
-        type: 'text'
-      });
-    };
-
-    this.addClinicalField = function() {
-      this.registry.clinicalFields.push({
+    this.addField = function(type) {
+      this.registry.fields.push({
         name: '',
         value: '',
         type: 'text',
-        length: 0
+        length: 255,
+        regType: type
       });
+
+      console.log(this.registry.fields);
     };
   }
 
