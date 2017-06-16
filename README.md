a6b7d11cc74d6e0168874623441302cb9e969b4a
Costin <costinaldea@yahoo.co.uk>
2017-06-16 10:35:31 +0300

    add case

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ### grunt 
 ### npm install http-server
 ### PORT=3001 hs
+
+### docker run --name mongo -v  -p 27017:27017 -d mongo:latest
+### docker start mongo
--- src/registries/registries.show.controller.js
+++ src/registries/registries.show.controller.js
@@ -5,14 +5,40 @@
     .module('colegiul-pacientilor.registries.controller')
     .controller('RegistriesShowCtrl', RegistriesShowCtrl);
 
-  RegistriesShowCtrl.$inject = ['RegistryService', '$state'];
+  RegistriesShowCtrl.$inject = ['RegistryService', '$state', '$http'];
 
-  function RegistriesShowCtrl(RegistryService, $state) {
+  function RegistriesShowCtrl(RegistryService, $state, $http    ) {
     var self = this;
 
     self.gridOptions = {
       enableSorting: true
     };
+
+
+      self.caseSubmit = function() {
+        var data  = { name: 'meaningless name'};
+
+          data.values = self.registry.fields.map( function (p) {
+              return { name: p.name, value: p.value };
+          });
+
+        console.log( JSON.stringify( data ) );
+
+          $http({
+              method: 'POST',
+              url: 'http://localhost:3001/' + self.registry._id + "/records",
+              data: data,
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          }).then(function successCallback(response) {
+              console.log(response);
+
+              $state.go('index.home');
+          }, function errorCallback(response) {
+                console.log(response);
+          });
+      };
 
     RegistryService.findById($state.params.id).then(function(response) {
       self.registry = response.data;
